b3871487197eaeb11b2803e79d41fbfaf152f181
MustafaAlneami <mero7383@gmail.com>
2025-04-16 17:35:17 +0300

    morny commit

---
+++ lib/presentation/screens/calender/calender_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalenderView extends StatefulWidget {
+  const CalenderView({super.key});
+
+  @override
+  State<CalenderView> createState() => _CalenderView();
+}
+
+class _CalenderView extends State<CalenderView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
