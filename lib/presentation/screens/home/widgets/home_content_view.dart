8393023d62327e2a38e38c3ac51f4822e3f6746a
MustafaAlneami <mero7383@gmail.com>
2025-04-18 01:49:59 +0300

    nighty commit

--- lib/presentation/screens/home/widgets/home_content_view.dart
+++ lib/presentation/screens/home/widgets/home_content_view.dart
@@ -11,10 +11,10 @@ class _HomeContentView extends State<HomeContentView> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 370,
+      height: 430,
       width: 370,
       child: Card(
-        color: Color(0xFFD8F3DC),
+        color: Color(0xFFEDEBFA),
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
@@ -42,7 +42,10 @@ class _HomeContentView extends State<HomeContentView> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(5)),
                         color: Color(0xFFE9F5EC),
-                        child: Icon(Icons.book),
+                        child: Image.asset(
+                          'images/yt logo.avif',
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
                   ),
@@ -55,23 +58,21 @@ class _HomeContentView extends State<HomeContentView> {
                       child: Card(
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(5)),
-                        color: Color(0xFFE9F5EC),
+                        color: Color(0xFFEDEBFA),
                         child: Text(' 10:00'),
                       ),
                     ),
                   ),
                 ],
               ),
-              Container(
+              SizedBox(
                 width: 330,
                 height: 30,
-                color: Color(0xFFE9F5EC),
                 child: Text('I Let ChatGPT Control My Life for 24 Hours '),
               ),
-              Container(
+              SizedBox(
                 width: 330,
                 height: 30,
-                color: Color(0xFFD8F3DC),
                 child: Row(
                   children: [
                     Image.asset(
@@ -81,28 +82,35 @@ class _HomeContentView extends State<HomeContentView> {
                     Text(
                       ' Kurzgesagt – In a Nutshell',
                       style: TextStyle(fontWeight: FontWeight.bold),
-                    )
+                    ),
                   ],
                 ),
               ),
+              SizedBox(
+                height: 10,
+              ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
                     alignment: Alignment.center,
-                    width: 165,
+                    margin: EdgeInsets.all(1),
+                    width: 160,
                     height: 40,
-                    color: Color(0xFF95D5B2),
+                    color: Color(0xFFF6C1D7),
                     child: Text(
-                      'RECO',
-                      style: TextStyle(fontSize: 20),
+                      'RECO ',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                   Container(
                     alignment: Alignment.center,
                     margin: EdgeInsets.all(1),
-                    width: 165,
+                    width: 160,
                     height: 40,
-                    color: Color(0xFF95D5B2),
+                    color: Color(0xFFFFD8B1),
                     child: Text(
                       'Watch ',
                       style: TextStyle(
@@ -111,7 +119,22 @@ class _HomeContentView extends State<HomeContentView> {
                     ),
                   )
                 ],
-              )
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)),
+                color: Color.fromARGB(255, 166, 149, 241),
+                child: Container(
+                  margin: EdgeInsets.all(1),
+                  alignment: Alignment.center,
+                  width: 350,
+                  height: 40,
+                  child: Text(
+                    '3th june 25 , 10:00 AM',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
