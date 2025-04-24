9d736524631e1e043852390b6858cad9ffc58a50
MustafaAlneami <mero7383@gmail.com>
2025-04-24 20:12:22 +0300

    nighty commit

--- lib/presentation/screens/calender/calender_view.dart
+++ lib/presentation/screens/calender/calender_view.dart
@@ -1,4 +1,6 @@
 // TODO Implement this library.
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -11,42 +13,60 @@ class CalenderView extends StatefulWidget {
 }
 
 class _CalenderView extends State<CalenderView> {
-  Future<void> _launchURL(String url) async {
-    final Uri uri = Uri.parse(url);
-    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-      throw Exception('Could not launch $url');
-    }
-  }
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
-    // return Column(
-    //   children: [
-    //     SizedBox(
-    //       height: 200,
-    //       width: 200,
-    //       child: Image.network(
-    //         'https://drive.google.com/uc?export=view&id=1_WrACPPdvIIIObhiwh1pIBa8J7P-taDW',
-    //         fit: BoxFit.fill,
-    //       ),
-    //     ),
-    //     SizedBox(
-    //       height: 200,
-    //       width: 200,
-    //       child: Image.network(
-    //         'https://drive.google.com/uc?export=view&id=1X5LeOnVkN0LJKPpo-khrv4N6riX5IdfI',
-    //         fit: BoxFit.fill,
-    //       ),
-    //     ),
-    //   ],
-    // );
-    return ListView(
+    return Column(
       children: [
         TableCalendar(
           firstDay: DateTime.utc(2010, 10, 16),
           lastDay: DateTime.utc(2030, 3, 14),
           focusedDay: DateTime.now(),
-        )
+          //Adding the following code to the calendar widget
+          // will allow it to respond to user's taps,
+          //marking the tapped day as selected:
+          selectedDayPredicate: (day) {
+            // Use `selectedDayPredicate` to determine which day is currently selected.
+            // If this returns true, then `day` will be marked as selected.
+
+            // Using `isSameDay` is recommended to disregard
+            // the time-part of compared DateTime objects.
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              // Call `setState()` when updating the selected day
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          },
+          //In order to dynamically update visible calendar format,
+          //add those lines to the widget:
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          },
+          //Add this one callback to complete the basic setup:
+          // onPageChanged: (focusedDay) {
+          //   _focusedDay = focusedDay;
+          // },
+          eventLoader: (day) {
+            // Return a list of events for the given day
+            return [
+              'Event 1',
+              'Event 2',
+              'Event 3',
+            ];
+          },
+          // Add this to make the calendar responsive to taps
+        ),
       ],
     );
   }
