92f162386373160aa30fab850f875e50ed60e6c0
MustafaAlneami <mero7383@gmail.com>
2025-04-24 23:16:08 +0300

    nighty commit

---
+++ lib/presentation/screens/calender/calendar_event_custom_list_tile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CalenderEventCustomlisttile extends StatefulWidget {
+  const CalenderEventCustomlisttile({super.key});
+
+  @override
+  State<CalenderEventCustomlisttile> createState() =>
+      _CalenderEventCustomlisttile();
+}
+
+class _CalenderEventCustomlisttile extends State<CalenderEventCustomlisttile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
