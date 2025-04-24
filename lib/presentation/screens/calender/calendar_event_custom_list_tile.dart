57083cff571f6d03b5a6bb773376e8f100f21b23
MustafaAlneami <mero7383@gmail.com>
2025-04-25 00:28:37 +0300

    nighty commit

--- lib/presentation/screens/calender/calendar_event_custom_list_tile.dart
+++ lib/presentation/screens/calender/calendar_event_custom_list_tile.dart
@@ -11,6 +11,40 @@ class CalenderEventCustomlisttile extends StatefulWidget {
 class _CalenderEventCustomlisttile extends State<CalenderEventCustomlisttile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          height: 115,
+          child: Card(
+            // elevation: 6,
+            margin: EdgeInsets.all(15),
+            surfaceTintColor: Colors.indigo,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                    leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset('images/creator3.jpg')),
+                    title: Text(
+                      'Lets Go to the Event',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text('De Musto'),
+                    trailing: Image.asset(
+                      'images/play.png',
+                      fit: BoxFit.fill,
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/presentation/screens/setting/setting_view.dart
+++ lib/presentation/screens/setting/setting_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reco_is_here/presentation/screens/calender/calendar_event_custom_list_tile.dart';
 
 class SettingView extends StatefulWidget {
   const SettingView({super.key});
@@ -10,6 +11,6 @@ class SettingView extends StatefulWidget {
 class _SettingView extends State<SettingView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CalenderEventCustomlisttile();
   }
 }
