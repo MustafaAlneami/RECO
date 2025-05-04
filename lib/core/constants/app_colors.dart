c90af270c161233744ed4e071b9e2178c45c5277
MustafaAlneami <mero7383@gmail.com>
2025-05-04 18:58:29 +0300

    calender bugs stuff commit

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //indigo colors
+  static const Color primaryColor = Colors.indigo;
+  static const Color primaryColorTwo = Color(0xFF007BFF);
+  //pinky 0xFFF8BBD0
+  static const Color pinkyone = Color(0xFFF8BBD0);
+  static Color? pinkyTwo = Colors.pink[100];
+  //New Released Color
+  static const Color newReleasedColor = Colors.red;
+  //Classic Gems Color
+  static const Color classicGemsColor = Colors.green;
+}
